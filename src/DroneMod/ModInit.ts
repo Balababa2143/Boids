f8b192cd414da26f9cebdb1e58d898aa1483d401
Balababa <balababa2143@gmail.com>
2025-07-15 14:57:09 -0700

    added some vibes

--- src/DroneMod/ModInit.ts
+++ src/DroneMod/ModInit.ts
@@ -328,3 +328,94 @@ import * as FaceCover from './Futuristic/Gag/FaceCover'
         }
     })
 })();
+
+(() => {
+    const r1 = <restraint>KinkyDungeonRestraints.find(r => r.name === 'NippleClamps')
+    const r2 = <restraint>KinkyDungeonRestraints.find(r => r.name === 'TrapVibe')
+    const r3 = <restraint>{
+        ...r2,
+        name: 'LockVibe',
+        escapeChance: {
+            ...r1!.escapeChance,
+            Struggle: r2!.escapeChance.Struggle
+        }
+    }
+    KinkyDungeonRestraints.push(r3)
+    KinkyDungeonAddRestraintText(
+        r3.name,
+        'Lockable Vibe',
+        TextProvider.instance.getText(`Restraint${r2!.name}Desc`),
+        TextProvider.instance.getText(`Restraint${r2!.name}Desc2`),
+    )
+    const r4 = <restraint>{
+        inventory: true,
+        arousalMode: true,
+        name: "DenialPlugF",
+        Asset: "VibratingDildo",
+        Color: "Default",
+        Group: "ItemVulva",
+        plugSize: 1.5,
+        power: 3,
+        weight: 2,
+        failSuffix: { "Struggle": "Plug" },
+        limited: true,
+        escapeChance: { ...r1.escapeChance, "Struggle": 0.25, Remove: 0.5 },
+        enemyTags: { "trap": 10, "maidRestraintsLight": 2, "genericToys": 2, 'machinePlug': 5, "toyPleasure": 2 },
+        playerTags: { "NoVibes": -1000 },
+        minLevel: 0,
+        allFloors: true,
+        shrine: ["Plugs", "Vibes", "Toys"],
+        // linkedVibeTags: ["plugs"],
+        allowRemote: true, events: [
+            { trigger: "beforeStruggleCalc", type: "vibeStruggle", inheritLinked: true },
+            { trigger: "remoteVibe", type: "RemoteActivatedVibe", power: 4, time: 5, edgeOnly: true },
+            { trigger: "tick", type: "PeriodicDenial", power: 1, time: 13, edgeOnly: true, cooldown: { "normal": 15, "tease": 10 }, chance: 0.02 },
+            { trigger: "tick", type: "PeriodicDenial", power: 2, time: 10, edgeOnly: true, cooldown: { "normal": 15, "tease": 10 }, chance: 0.025 },
+            { trigger: "tick", type: "PeriodicDenial", power: 3, time: 7, edgeOnly: true, cooldown: { "normal": 15, "tease": 10 }, chance: 0.03 },
+            { trigger: "tick", type: "PeriodicDenial", power: 4, time: 4, edgeOnly: true, cooldown: { "normal": 15, "tease": 10 }, chance: 0.015 },
+            { trigger: "tick", type: "PeriodicDenial", power: 5, time: 6, edgeOnly: true, cooldown: { "normal": 15, "tease": 10 }, chance: 0.075 },
+            { trigger: "tick", type: "PeriodicTeasing", power: 5, time: 3, edgeOnly: false, cooldown: { "normal": 15, "tease": 10 }, chance: 0.0025 },
+        ]
+    }
+    KinkyDungeonRestraints.push(r4 as restraint)
+    KinkyDungeonAddRestraintText(
+        r4.name,
+        'Lockable Plug',
+        TextProvider.instance.getText(`Restraint${'TrapPlug3'}Desc`),
+        TextProvider.instance.getText(`Restraint${'TrapPlug3'}Desc2`),
+    )
+    const r5 = <restraint>{
+        inventory: true,
+        arousalMode: true,
+        name: "DenialPlugR",
+        Asset: "VibratingDildoPlug",
+        Color: "Default",
+        Group: "ItemButt",
+        plugSize: 2.0,
+        power: 5,
+        weight: 1,
+        escapeChance: { ...r1.escapeChance, "Struggle": 0.25, Remove: 0.5 },
+        enemyTags: { "genericToys": 1, "rearToys": 10, "toyEdgeMid": 2 },
+        limited: true,
+        playerTags: { "NoVibes": -1000 },
+        minLevel: 0,
+        allFloors: true,
+        shrine: ["Plugs", "Vibes", "Toys"],
+        failSuffix: { "Struggle": "Plug" },
+        // linkedVibeTags: ["plugs"],
+        allowRemote: true,
+        events: [
+            { trigger: "beforeStruggleCalc", type: "vibeStruggle", inheritLinked: true },
+            { trigger: "remoteVibe", type: "RemoteActivatedVibe", power: 2, time: 30, edgeOnly: true },
+            { trigger: "tick", type: "PeriodicDenial", power: 1, time: 24, edgeOnly: true, cooldown: { "normal": 50, "tease": 20 }, chance: 0.03 },
+            { trigger: "tick", type: "PeriodicDenial", power: 2, time: 16, edgeOnly: true, cooldown: { "normal": 50, "tease": 20 }, chance: 0.02 },
+        ]
+    }
+    KinkyDungeonRestraints.push(r5 as restraint)
+    KinkyDungeonAddRestraintText(
+        r5.name,
+        'Lockable Rear Plug',
+        TextProvider.instance.getText(`Restraint${'RearVibe1'}Desc`),
+        TextProvider.instance.getText(`Restraint${'RearVibe1'}Desc2`),
+    )
+})();
