c1d8da22f0303c3e6a1f390e3db0cd33d56aeeeb
Balababa <balababa2143@gmail.com>
2025-08-22 22:42:02 -0700

    refactor: enhance property access in CreateWeakAction to bind functions to target

--- src/Utilities/WeakDelegate.ts
+++ src/Utilities/WeakDelegate.ts
@@ -86,6 +86,15 @@ export const CreateWeakAction = <TArgs extends unknown[]>() =>
             apply(target, _, argArray: TArgs): void {
                 target.Invoke(...argArray)
             },
+            get(target, p, receiver) {
+                const value = Reflect.get(target, p, receiver)
+                if (typeof value === 'function'){
+                    return value.bind(target)
+                }
+                else{
+                    return value
+                }
+            },
         }
     ) as unknown as IMulticastAction<TArgs> // Type assertion to match the interface
     
